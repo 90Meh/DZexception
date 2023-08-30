e151a0e1c00b686259dc594d31965e96b43017e0
meh <meh@IT5>
2023-08-30 10:23:29 +0800

    Работающая менюшка

--- DZexception/Program.cs
+++ DZexception/Program.cs
@@ -4,32 +4,137 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var a = "";
+        var b = "";
+        var c = "";
+        string[] menuItems = new string[] { "a", "b", "c" };
+        int row = Console.CursorTop;
+        int col = Console.CursorLeft;
+        int index = 0;
+        
+        
 
         while (true)
         {
+            Console.WriteLine($"\n\n\n\n{a} * x^2 + {b} * x + {c} = 0");
+
             try
             {
-                var input = StartInput();
-                var a = input.a;
-                var b = input.b;
-                var c = input.c;
 
-                var sol = Solution(a, b, c);
-                if (sol.D > 0)
+
+                
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                DrawMenu(menuItems, row, col, index);
+
+
+                switch (Console.ReadKey(true).Key)
                 {
-                    Console.WriteLine("x1= {0}\nx2= {1}", sol.x1, sol.x2);
+                    case ConsoleKey.DownArrow:
+                        if (index < menuItems.Length - 1)
+                            index++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (index > 0)
+                            index--;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        var resultParse = Processing(a, b, c);
+                        var sol = Solution(resultParse.a, resultParse.b, resultParse.c);
+                        Console.WriteLine($"\n{a} * x^2 + {b} * x + {c} = 0");
+                        if (sol.D > 0)
+                        {
+                            Console.WriteLine("x1= {0}\nx2= {1}", sol.x1, sol.x2);
+                        }
+                        else
+                        {
+                            Console.WriteLine("x= {0}", sol.x1);
+                        }
+                        Console.ReadLine();
+                        Console.Clear();
+                        a = "";
+                        b = "";
+                        c = "";
+                        break;
+                    default:
+                        switch (index)
+                        {
+                            case 0:
+                                a = a + Console.ReadKey().KeyChar;
+                                break;
+                            case 1:
+                                b = b + Console.ReadKey().KeyChar;
+                                break;
+                            case 2:
+                                c = c + Console.ReadKey().KeyChar;
+                                break;
+                        }
+
+                        break;
                 }
-                else
-                {
-                    Console.WriteLine("x= {0}", sol.x1);
-                }
+
+                
 
             }
             catch (Exception Ex)
             {
+
                 Console.WriteLine(Ex.Message);
+                Console.ReadLine();
+                Console.Clear();
+                a = "";
+                b = "";
+                c = "";
             }
+
         }
+
+        //while (true)
+        //{
+        //    try
+        //    {
+        //        var input = StartInput();
+        //        var a = input.a;
+        //        var b = input.b;
+        //        var c = input.c;
+
+        //        var sol = Solution(a, b, c);
+        //        if (sol.D > 0)
+        //        {
+        //            Console.WriteLine("x1= {0}\nx2= {1}", sol.x1, sol.x2);
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("x= {0}", sol.x1);
+        //        }
+
+        //    }
+        //    catch (Exception Ex)
+        //    {
+        //        Console.WriteLine(Ex.Message);
+        //    }
+        //}
+    }
+    private static void DrawMenu(string[] items, int row, int col, int index)
+    {
+        Console.SetCursorPosition(col, row);
+        for (int i = 0; i < items.Length; i++)
+        {
+
+            if (i == index)
+            {
+                Console.WriteLine($">{items[i]}");
+            }
+            else
+            {
+                Console.WriteLine($" {items[i]}");
+            }
+
+        }
+        Console.WriteLine();
     }
 
 
@@ -57,7 +162,8 @@ internal class Program
             Console.WriteLine($"Неверный формат параметра {perem}");
             Console.WriteLine($" a = {a} \n b = {b} \n c = {c}");
             Console.ResetColor();
-            return StartInput();
+            return (0,0,0);
+            //return StartInput();
         }
     }
 
